57b127b426111167cc4d74cfc4f06996834e6b74
Văn Thiết <tretrauthiet@gmail.com>
2024-04-18 20:43:11 +0700

    up

--- client/src/components/Ui-Books/InforBookUi.jsx
+++ client/src/components/Ui-Books/InforBookUi.jsx
@@ -3,18 +3,15 @@ import { Link } from "react-router-dom";
 import ReadBookUi from "../button-ui/ReadBookUi";
 const Infor = (props) => {
     const { imgBook, nameBook, author, labelBook, descriptionBook, bookId } = props;
-    // 
     return (
         <>
             <div className={` bg-[#1a1a1c] w-[104%] h-[auto]  rounded-[10px] opacity-[0.9] blur-[0.5px] overflow-hidden p-4 px-4 backdrop-filter backdrop-blur-xl backdrop-saturate-150 backdrop-grayscale-50  `}>
                 <Link to={`/ebook/${bookId}`}>
                     <div className="w-[700px] flex items-center gap-4 ">
-                        {/* flex-row-reverse */}
                         <img loading="lazy" className="rounded-[10px] w-[250px] h-[350px] object-cover" src={imgBook} alt="" />
                         <div className="w-full">
                             <h1 className="text-white font-bold cursor-pointer text-[14px] w-[80%] py-4">{nameBook} </h1>
                             <span className="text-white">{author}</span>
-
                             <div className="flex w-full justify-between items-center gap-2 py-5">
                                 <div className="flex w-full justify-between gap-3 items-center">
                                     <div>
