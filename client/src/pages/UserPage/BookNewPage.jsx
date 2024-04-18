5da1d68105d074757f1c680bfa1eae10f5a43a1d
Văn Thiết <tretrauthiet@gmail.com>
2024-04-18 19:45:14 +0700

    update

--- client/src/pages/UserPage/BookNewPage.jsx
+++ client/src/pages/UserPage/BookNewPage.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 import Header from "~/layouts/Header";
 import BookNewUi from "~/layouts/Contents/BookNewUi";
-import Card from "../../components/cardBook/card";
-import { GetInforBook, BookNewApi } from "../../services/books/Book";
+import Card from "@components/cardBook/card";
+import { GetInforBook, BookNewApi } from "@services/books/Book";
 import Footer from "~/layouts/Footer";
-import TitleSetter from "../../components/titlePage/TitleSetter";
+import TitleSetter from '@components/titlePage/TitleSetter';
+
 const BookNewPage = () => {
   const [cards, setCards] = useState([]);
   useEffect(() => {
--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
   },
   resolve:{
     alias:{
-      '~': '/src'
+      '~': '/src',
+      '@components': '/src/components'
     }
   }
 })
