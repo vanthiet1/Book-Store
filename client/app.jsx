b1059ef042005aa9266637a791046c45a2acd1c1
Văn Thiết <tretrauthiet@gmail.com>
2024-04-22 13:42:44 +0700

    up

--- client/app.jsx
+++ client/app.jsx
@@ -14,6 +14,7 @@ const App = () => {
   const { filterAdmin } = useContext(DisplayPopup);
   const privateRouterAdmin = PublicRouterAdmin();
     console.log('CCCCCCCC');
+    
     const loginComponent = useMemo(() => {
       return displayLogin ? <Login /> : null;
     }, [displayLogin]);
