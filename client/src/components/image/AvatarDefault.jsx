7a78c5715e80d81d5cbe68fed479ad292654db30
Văn Thiết <tretrauthiet@gmail.com>
2024-04-23 17:11:29 +0700

    change link avartar

--- client/src/components/image/AvatarDefault.jsx
+++ client/src/components/image/AvatarDefault.jsx
@@ -1,4 +1,4 @@
-const AvatarUser= "https://res.cloudinary.com/dz93cdipw/image/upload/v1713851345/Book-Store/avatar-user/avartar-default.png";
+const AvatarUser= "https://res.cloudinary.com/dz93cdipw/image/upload/v1713866997/Book-Store/Avatar/kwuemqemetzsp4jw21mt.webp";
 export {
     AvatarUser
 }
