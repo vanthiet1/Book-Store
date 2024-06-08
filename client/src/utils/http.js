6b233a8e4cb89e8cc6e324fae4b1ead637fe564f
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2024-06-08 18:25:52 +0700

    Update http.js

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -3,13 +3,10 @@ import axios from 'axios';
 class Http {
     instance;
     constructor(){
-<<<<<<< HEAD
          this.instance = axios.create({ 
-            baseURL:'http://localhost:8080/api/v1/',
-=======
-         this.instance = axios.create({
+            // baseURL:'http://localhost:8080/api/v1/',
+            this.instance = axios.create({
             baseURL:'https://book-store-9po5.onrender.com/api/v1/',
->>>>>>> 7cfb221b0d1aee8a5085275b33def3c2fef5b8a1
             timeout:10000,
             headers:{
                 'Content-Type': 'application/json'
