996619d1c7302980b6599a899a58f32a27fe9a12
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2024-06-08 18:33:11 +0700

    Update http.js

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -2,17 +2,18 @@ import axios from 'axios';
 
 class Http {
     instance;
-    constructor(){
-         this.instance = axios.create({ 
-            // baseURL:'http://localhost:8080/api/v1/',
-            this.instance = axios.create({
-            baseURL:'https://book-store-9po5.onrender.com/api/v1/',
-            timeout:10000,
-            headers:{
+    constructor() {
+// baseURL:'http://localhost:8080/api/v1/',
+        
+        this.instance = axios.create({ 
+            baseURL: 'https://book-store-9po5.onrender.com/api/v1/',
+            timeout: 10000,
+            headers: {
                 'Content-Type': 'application/json'
             }
-         })
+        });
     }
 }
+
 const http = new Http().instance;
 export default http;
