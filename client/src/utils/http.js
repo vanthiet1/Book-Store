4dcc888613b63f968c053c9963bee8361efa0668
Văn Thiết ( Frontend DEV ) <125172255+vanthiet1@users.noreply.github.com>
2025-05-13 11:10:09 +0700

    Update http.js

--- client/src/utils/http.js
+++ client/src/utils/http.js
@@ -4,9 +4,8 @@ class Http {
     instance;
     constructor() {
 // baseURL:'http://localhost:8080/api/v1/',
-        
         this.instance = axios.create({ 
-            baseURL: 'https://book-store-production-839e.up.railway.app/api/v1/',
+            baseURL: 'https://book-store-production-1713.up.railway.app/api/v1/',
             timeout: 10000,
             headers: {
                 'Content-Type': 'application/json'
